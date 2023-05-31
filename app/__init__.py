8e35fc8bcf3a70bb6c98838f4bf5f3a73091d8c8
Kevin Wang <kwang30@stuy.edu>
2023-05-30 22:59:38 -0400

    cd functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import fileman
 import os
 import terminal
-from subprocess import Popen
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12).hex()
@@ -12,19 +11,42 @@ def connectForm():
     #basic http form that takes in username and password
     return render_template("connect.html")
 
-@app.route("/server", methods=["POST"])
-def server():
+@app.route("/connect", methods=["POST"])
+def connect():
     #connect to server
     #get username and password from post form
     username = request.form["username"]
     password = request.form["password"]
     
-    fileman.init_client(username, password)
-    files = fileman.list_files(username)
-
     terminal.startTerminal(username, password)
+    fileman.init_client(username, password)
+    session["username"] = username
 
-    return render_template("server.html", files = files)
+    return redirect("/file_system")
+
+@app.route("/file_system", methods=["GET"])
+def file_system():
+    files = fileman.list_files(session["username"])
+    folders = fileman.list_folders(session["username"])
+
+    return render_template("file_system.html", username = session["username"], files = files, folders = folders, currentPath=fileman.currentPath(session["username"]))
+
+@app.route("/getFileData", methods=["GET"])
+def getFileData():
+    #get file data from server
+    username = request.args.get("username")
+    filename = request.args.get("filename")
+    
+    data = fileman.cat_file(filename, username)
+    return data
+
+@app.route("/cd", methods=["GET"])
+def cd():
+    #change directory
+    username = request.args.get("username")
+    directory = request.args.get("directory")
+    fileman.cd(directory, username)
+    return "Success"
 
 if __name__ == "__main__":
     app.debug = False
--- app/fileman.py
+++ app/fileman.py
@@ -3,6 +3,7 @@ from flask import session
 import os
 
 clients = {}
+client_directory = {}
 
 def init_client(username, password):
     proxy = None
@@ -14,17 +15,40 @@ def init_client(username, password):
 
     client.connect(server, username=username, password=password, sock=proxy)
     clients[username] = client
-    session["username"] = username
+
+    client_directory[username] = "~/"
 
 def list_files(username):
 
-    ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command("ls -a")
+    ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command( f"cd {client_directory[username]}; ls -p | grep -v /")
 
     return ssh_stdout.readlines()
 
+def list_folders(username):
+
+    ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command(f"cd {client_directory[username]}; ls -d */")
+
+    return ssh_stdout.readlines()
+
+def cat_file(file, username):
+
+    ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command(f"cd {client_directory[username]}; cat {file}")
+
+    return ssh_stdout.readlines()
+
+def cd(directory, username):
+    print(directory)
+    new_directory = client_directory[username] + "/" + directory
+    print(new_directory)
+    ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command(f"cd {new_directory}; pwd")
+    client_directory[username] = ssh_stdout.readlines()[0][:-1]
+    print(client_directory[username])
+
+def currentPath(username):
+    return client_directory[username]
 
 def rename_file(directory, old, new, username):
 
     ssh_stdin, ssh_stdout, ssh_stderr = clients[username].exec_command("mv " + directory + "/" + old + " " + directory + "/" + new)
-    
+
     return ssh_stdout.readlines()
